02c3cb78fe7201a9139b5e68c11d18abd0dc355b
Code Wolf <brgerner@gmail.com>
2021-06-17 15:43:46 -0700

    controllers, views, public - todo: only show posts by current user on dashboard, set timer for automatic logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -57,15 +57,9 @@ router.get("/post/:id", (req, res) => {
     },
     attributes: [
       "id",
-      "post_url",
       "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
+      "description",
+      "created_at"
     ],
     include: [
       {
